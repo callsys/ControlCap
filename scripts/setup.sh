47d96a8f8f273d796a0176f7f7b8bafc9d5b9a4a
Yuzhong Zhao <48741629+callsys@users.noreply.github.com>
2024-05-26 00:38:41 +0800

    Add files via upload

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+pip install --upgrade pip
+pip install salesforce-lavis==1.0.2
+pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu121
+pip install scikit-learn==1.3.2
+pip install SceneGraphParser==0.1.0
+python -m spacy download en
+pip install textblob==0.17.1
+pip install peft==0.8.2
+pip install DCNv4==1.0.0.post2
