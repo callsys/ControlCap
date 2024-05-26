aa3074ffd2042b046fed5da6143738cb553e5937
Yuzhong Zhao <48741629+callsys@users.noreply.github.com>
2024-05-26 13:08:06 +0800

    Update train_vg1.2_refcocog.sh

--- scripts/train/train_vg1.2_refcocog.sh
+++ scripts/train/train_vg1.2_refcocog.sh
@@ -3,4 +3,4 @@
 ls
 
 # train controlcap on vg1.2 + refcocog
-python -m torch.distributed.run --nproc_per_node=8 --master_port=29600 train.py --cfg-path configs/train/union/vg_refcocog_5e.yaml
+python -m torch.distributed.run --nproc_per_node=8 --master_port=29600 train.py --cfg-path configs/train/union/vg1.2_refcocog_5e.yaml
